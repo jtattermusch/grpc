560073dc5e6757a7bd6a521a524e607c3a8a054c
Jan Tattermusch <jtattermusch@google.com>
2017-06-05 14:06:07 -0700

    pfdsfafd

--- test/core/end2end/invalid_call_argument_test.c
+++ test/core/end2end/invalid_call_argument_test.c
@@ -610,7 +610,7 @@ static void test_invalid_initial_metadata_reserved_key() {
 int main(int argc, char **argv) {
   grpc_test_init(argc, argv);
   grpc_init();
-  /*test_invalid_initial_metadata_reserved_key();
+  test_invalid_initial_metadata_reserved_key();
   test_non_null_reserved_on_start_batch();
   test_non_null_reserved_on_op();
   test_send_initial_metadata_more_than_once();
@@ -622,14 +622,14 @@ int main(int argc, char **argv) {
   test_receive_message_with_invalid_flags();
   test_receive_two_messages_at_the_same_time();
   test_recv_close_on_server_from_client();
-  test_recv_status_on_client_twice();*/
+  test_recv_status_on_client_twice();
   test_send_close_from_client_on_server();
-  /*test_recv_status_on_client_from_server();
+  test_recv_status_on_client_from_server();
   test_send_status_from_server_with_invalid_flags();
   test_too_many_trailing_metadata();
   test_send_server_status_twice();
   test_recv_close_on_server_with_invalid_flags();
-  test_recv_close_on_server_twice();*/
+  /*test_recv_close_on_server_twice();*/
   grpc_shutdown();
 
   return 0;

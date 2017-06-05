9044ba1d79fde4b20e4f14dbc9b6969f219e5b6b
Jan Tattermusch <jtattermusch@google.com>
2017-06-05 10:27:03 -0700

    only invalid_call_argument_test

--- tools/run_tests/run_tests_matrix.py
+++ tools/run_tests/run_tests_matrix.py
@@ -133,7 +133,7 @@ def _create_test_jobs(extra_args=[], inner_jobs=_DEFAULT_INNER_JOBS):
                              configs=['dbg', 'opt'],
                              platforms=['linux', 'macos', 'windows'],
                              labels=['basictests'],
-                             extra_args=extra_args,
+                             extra_args=extra_args + ['-r', 'invalid_call_argument_test'],
                              inner_jobs=inner_jobs)
 
   # supported on linux and mac.

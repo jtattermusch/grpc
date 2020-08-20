bd648e697ccc2c3fbfabe34ed0192266aa699c5f
Jan Tattermusch <jtattermusch@google.com>
2020-08-20 10:54:12 +0200

    cleanup bazel commands in test scripts (for non-RBE invocations)

--- tools/internal_ci/macos/grpc_run_bazel_isolated_tests.sh
+++ tools/internal_ci/macos/grpc_run_bazel_isolated_tests.sh
@@ -21,6 +21,7 @@ cd $(dirname $0)/../../..
 ./tools/run_tests/start_port_server.py
 
 # run cfstream_test separately because it messes with the network
+# TODO(jtattermusch): "standalone" is deprecated in bazel. Is it actually required for the tests to pass?
 tools/bazel test $RUN_TESTS_FLAGS --spawn_strategy=standalone --genrule_strategy=standalone --test_output=all --copt="-DGRPC_CFSTREAM=1" //test/cpp/end2end:cfstream_test
 
 # Make sure time is in sync before running time_jump_test because the test does
@@ -28,6 +29,7 @@ tools/bazel test $RUN_TESTS_FLAGS --spawn_strategy=standalone --genrule_strategy
 sudo sntp -sS pool.ntp.org
 
 # run time_jump_test separately because it changes system time
+# TODO(jtattermusch): "standalone" is deprecated in bazel. Is it actually required for the tests to pass?
 tools/bazel test $RUN_TESTS_FLAGS --spawn_strategy=standalone --genrule_strategy=standalone --test_output=all //test/cpp/common:time_jump_test
 
 # kill port_server.py to prevent the build from hanging

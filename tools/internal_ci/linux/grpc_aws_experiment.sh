714e2a8f53e0720a39191ba83744d8c98e249cfc
Alexander Midlash <amidlash@google.com>
2021-04-20 18:19:03 +0000

    debug2

--- tools/internal_ci/linux/grpc_aws_experiment.sh
+++ tools/internal_ci/linux/grpc_aws_experiment.sh
@@ -19,6 +19,6 @@ fi
 FILE=grpc_aws_experiment_remote.sh
 chmod 700 $IDENTITY
 pwd
-ls
+ls github
 scp -i $IDENTITY -o StrictHostKeyChecking=no tools/internal_cli/linux/$FILE ubuntu@$INSTANCE:
 ssh -i $IDENTITY -o StrictHostKeyChecking=no $INSTANCE "uname -a; ls -l; bash ./$FILE"

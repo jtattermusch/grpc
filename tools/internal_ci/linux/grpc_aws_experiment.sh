02f42c320724ae7987b798b205f9640d4acd35ab
Alexander Midlash <amidlash@google.com>
2021-04-20 18:26:30 +0000

    path fixes

--- tools/internal_ci/linux/grpc_aws_experiment.sh
+++ tools/internal_ci/linux/grpc_aws_experiment.sh
@@ -18,5 +18,6 @@ fi
 
 FILE=grpc_aws_experiment_remote.sh
 chmod 700 $IDENTITY
-scp -i $IDENTITY -o StrictHostKeyChecking=no github/grpc/tools/internal_cli/linux/$FILE ubuntu@$INSTANCE:
+ls github/grpc/
+scp -i $IDENTITY -o StrictHostKeyChecking=no github/grpc/tools/internal_ci/linux/$FILE ubuntu@$INSTANCE:
 ssh -i $IDENTITY -o StrictHostKeyChecking=no $INSTANCE "uname -a; ls -l; bash ./$FILE"

7cf572f0ffe94c3c8c23478a79a040e286c62898
Alexander Midlash <amidlash@google.com>
2021-04-21 17:23:17 +0000

    pr fixups: 1/x

--- tools/internal_ci/linux/grpc_aws_experiment.sh
+++ tools/internal_ci/linux/grpc_aws_experiment.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 # This script is responsible for remotely running tests on an ARM instance.
 # It should return a status code useful to the kokoro infrastructure.
@@ -18,16 +18,16 @@ fi
 
 FILE=grpc_aws_experiment_remote.sh
 chmod 700 $IDENTITY
-ssh -i $IDENTITY -o StrictHostKeyChecking=no ubuntu@$INSTANCE "rm -rf grpc"  # TODO: remove when using fresh instances
-scp -i $IDENTITY -o StrictHostKeyChecking=no -r github/grpc ubuntu@$INSTANCE:
-ssh -i $IDENTITY -o StrictHostKeyChecking=no ubuntu@$INSTANCE "uname -a; ls -l; bash grpc/tools/internal_ci/linux/$FILE" > ssh_log.txt
-cat ssh_log.txt
+REMOTE_SCRIPT_FAILURE=0
+ssh -i $IDENTITY -o StrictHostKeyChecking=no ubuntu@$INSTANCE "rm -rf grpc"
+scp -i $IDENTITY -o StrictHostKeyChecking=no -r github/grpc ubuntu@$INSTANCE
+ssh -i $IDENTITY -o StrictHostKeyChecking=no ubuntu@$INSTANCE "uname -a; ls -l; bash grpc/tools/internal_ci/linux/$FILE" || REMOTE_SCRIPT_FAILURE=$?
+
 # Sync back sponge_log artifacts (wip)
 echo "looking for sponge logs..."
 find . | grep sponge_log
 
 
 # Match return value
-RETURN=$(tail -n1 ssh_log.txt | sed "s/RETURN=//")
-echo "returning $RETURN based on script output"
-exit $RETURN
+echo "returning $REMOTE_SCRIPT_FAILURE based on script output"
+exit $REMOTE_SCRIPT_FAILURE
